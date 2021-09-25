fcdc63b91c813daa1ad75099243cb07911546d55
Einav1 <einavshpigel@gmail.com>
2021-09-25 13:24:41 +0300

    hide key shh..

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -4,7 +4,7 @@ import "firebase/storage";
 import "firebase/firestore";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyBelBngWlTfSAsIh74E-E3PxNdG9LsTMho",
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
